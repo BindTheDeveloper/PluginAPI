68df0753bcc7dbf63f174a449a458a6d51331d7f
Dan-Java <owensdaniel48@gmail.com>
2022-05-07 16:26:56 +0100

    Implemented delayed User Manager init with an Event to listen to

--- src/main/java/me/dan/pluginapi/PluginAPI.java
+++ src/main/java/me/dan/pluginapi/PluginAPI.java
@@ -5,10 +5,12 @@ import me.dan.pluginapi.configurable.Config;
 import me.dan.pluginapi.configurable.Messages;
 import me.dan.pluginapi.configuration.Configuration;
 import me.dan.pluginapi.configuration.Serialization;
+import me.dan.pluginapi.event.UserManagerLoadEvent;
 import me.dan.pluginapi.file.YamlFile;
 import me.dan.pluginapi.item.Item;
 import me.dan.pluginapi.plugin.CustomPlugin;
 import me.dan.pluginapi.user.UserManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
@@ -21,7 +23,10 @@ public final class PluginAPI extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        this.userManager = new UserManager();
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            this.userManager = new UserManager();
+            Bukkit.getServer().getPluginManager().callEvent(new UserManagerLoadEvent());
+        }, 20);
         Serialization.register(Item.class);
         Configuration.loadConfig(new YamlFile("config.yml", this.getDataFolder().getAbsolutePath(), null, this), Config.values());
         Configuration.loadConfig(new YamlFile("messages.yml", getDataFolder().getAbsolutePath(), null, this), Messages.values());
---
+++ src/main/java/me/dan/pluginapi/event/UserManagerLoadEvent.java
@@ -0,0 +1,19 @@
+package me.dan.pluginapi.event;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class UserManagerLoadEvent extends Event {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+}
