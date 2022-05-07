4d73a13f793d072951d74105d5e206f6f3f23b4e
Dan-Java <owensdaniel48@gmail.com>
2022-05-07 16:48:02 +0100

    Manager Class

---
+++ src/main/java/me/dan/pluginapi/manager/Manager.java
@@ -0,0 +1,35 @@
+package me.dan.pluginapi.manager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Manager<K, V> {
+
+    private final Map<K, V> map;
+
+    public Manager() {
+        this.map = new HashMap<>();
+    }
+
+    public void insert(K key, V value) {
+        map.put(keyConvert(key), value);
+    }
+
+    public void remove(K key) {
+        map.remove(keyConvert(key));
+    }
+
+    private K keyConvert(K key) {
+        if (key instanceof String) {
+            return (K) ((String) key).toLowerCase();
+        }
+        return key;
+    }
+
+    public V get(K key) {
+        return map.get(keyConvert(key));
+    }
+
+
+
+}
